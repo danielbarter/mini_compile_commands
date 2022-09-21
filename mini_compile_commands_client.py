266f252d59a014a01e7d123c4fcbb2e58d18a7f5
Daniel Barter <danielbarter@gmail.com>
2022-09-21 14:48:50 -0700

    server looking goog

--- mini_compile_commands_client.py
+++ mini_compile_commands_client.py
@@ -1,7 +1,8 @@
-#! @python@
+#! /run/current-system/sw/bin/python
 
 import socket
 import sys
+import pickle
 
 unix_socket_file = "/tmp/mini_compile_commands_unix_socket"
 
@@ -9,6 +10,6 @@ with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
     client.connect(unix_socket_file)
 
     for i in range(3):
-        client.send(sys.argv[1].encode('utf-8'))
+        client.send(pickle.dumps(sys.argv))
 
     client.close()
--- mini_compile_commands_server.py
+++ mini_compile_commands_server.py
@@ -1,28 +1,52 @@
-#! @python@
+#! /run/current-system/sw/bin/python
 
 from socketserver import UnixStreamServer, StreamRequestHandler
+from time import localtime, strftime
+import signal
 import os
+import sys
+import pickle
+import json
+
+if len(sys.argv) != 2:
+    print("usage: mini_compile_commands_client.py compile_commands.json")
+    sys.exit(1)
 
 unix_socket_file = "/tmp/mini_compile_commands_unix_socket"
+compile_commands_output = sys.argv[1]
 
-if os.path.isfile(unix_socket_file):
-    os.remove(unix_socket_file)
+def log_message(*args, **kwargs):
+    print(
+        '[' + strftime('%H:%M:%S', localtime()) + ']',
+        *args, **kwargs)
 
 class Handler(StreamRequestHandler):
     def handle(self):
-        msg = self.rfile.read()
-        print("added compile command:", msg)
-        self.server.compile_commands.append(msg)
+        compile_commands_binary = self.rfile.read()
+        compile_commands = pickle.loads(compile_commands_binary)
+        log_message("recived {} compile commands".format(len(compile_commands)))
+        self.server.compile_commands.extend(compile_commands)
 
 class Server(UnixStreamServer):
-
 
     def __init__(self, server_address, RequestHandlerClass):
         super().__init__(server_address, RequestHandlerClass)
         self.compile_commands = []
 
     def server_close(self):
-        print(self.compile_commands)
+        with open(compile_commands_output, 'w') as f:
+            f.write(json.dumps(self.compile_commands))
 
-with Server(unix_socket_file, Handler) as server:
-    server.serve_forever()
+        super().server_close()
+
+server = Server(unix_socket_file, Handler)
+
+def signal_handler(sig, frame):
+    print('')
+    server.server_close()
+    os.remove(unix_socket_file)
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+server.serve_forever()
