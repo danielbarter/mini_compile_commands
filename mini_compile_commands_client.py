c51c07e214525a984cdc23a225c702d0872b7a31
Daniel Barter <danielbarter@gmail.com>
2022-09-21 14:56:24 -0700

    client looking good

--- mini_compile_commands_client.py
+++ mini_compile_commands_client.py
@@ -3,13 +3,28 @@
 import socket
 import sys
 import pickle
+import re
 
 unix_socket_file = "/tmp/mini_compile_commands_unix_socket"
+directory = sys.argv[1]
+command = sys.argv[2::]
+
+c_file_regex = re.compile('.*\.cc$|.*\.c$|.*\.cpp$|.*\.cxx$')
+c_files = [ word for word in command if c_file_regex.search(word) ]
+compile_commands = []
+
+for c_file in c_files:
+    compile_commands.append(
+        { 'directory' : directory,
+          'command' : ' '.join(command),
+          'file' : c_file
+        }
+    )
 
 with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
     client.connect(unix_socket_file)
 
     for i in range(3):
-        client.send(pickle.dumps(sys.argv))
+        client.send(pickle.dumps(compile_commands))
 
     client.close()
