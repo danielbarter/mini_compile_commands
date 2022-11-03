4ebf743c09ca7723e24cd613f68d9bcc5f3cd610
Daniel Barter <danielbarter@gmail.com>
2022-11-02 19:32:15 -0700

    remove unneeded import

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "A tool for generating compile_commands.json files with nix";
   outputs = { self }: {
-    generator = import ./generator.nix;
+    generator = ./generator.nix;
   };
 }
