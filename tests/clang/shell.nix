e9405a00ec7869a76df64c76cfe70d385a6e02f5
Daniel Barter <danielbarter@gmail.com>
2022-12-24 18:08:06 -0800

    first step in standing up some tests

---
+++ tests/clang/shell.nix
@@ -0,0 +1,12 @@
+with import ( builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.11.tar.gz";
+  }) {};
+let mcc-env = (callPackage ../.. {}).wrap clangStdenv;
+in (mkShell.override {stdenv = mcc-env;}) {
+  shellHook = ''
+    mini_compile_commands_server.py &
+    sleep 1 # give the server some time to start up
+    $CXX test.cc -o /dev/null
+    kill -s SIGINT $!
+  '';
+}
---
+++ tests/clang/test.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "mini_compile_commands test\n";
+}
